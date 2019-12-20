ca85e1c840a04d4498c61c60438a31412d3a66d6
MegaLoler/Madeline/Aardbei <megaloler9000@gmail.com>
2019-12-19 18:00:11 -0600

    working

--- tblparser.py
+++ tblparser.py
@@ -11,12 +11,12 @@ def peek(stream):
     c = stream.peek()
     return c, len(c)
 
-def consume(stream, predicate, peek_func=None):
+def consume(stream, predicate, peek_func=peek):
     '''Consume bytes from a stream as long as a condition is met.'''
 
     token = ''
     while True:
-        chunk, size = (peek_func or peek)(stream)
+        chunk, size = peek_func(stream)
         if size and predicate(chunk):
             stream.read(size)
             token += chunk
@@ -71,7 +71,7 @@ def consume_delimited(stream, delimiter):
 
     return string
 
-def consume_token(stream):
+def consume_token(stream, peek_func=peek, discrete_tokens=''):
     '''Consume and return a single token from a stream.'''
 
     def peek_strip(stream):
@@ -81,7 +81,7 @@ def consume_token(stream):
         while stream.peek() == ';':
             consume_line(stream)
 
-        return peek(stream)
+        return peek_func(stream)
 
     def peek_token(stream):
         c = peek_strip(stream)[0]
@@ -95,19 +95,20 @@ def consume_token(stream):
         # check for string delimiters
         return peek_delimited() if c and c in ''''"''' else (c, len(c))
 
-    # newlines are their own token
-    # consolodate multiple newlines into one
-    if stream.peek() == '\n':
-        consume_in(stream, '\n')
-        return '\n'
+    # consolodate multiple discrete tokens and return them
+    for c in discrete_tokens:
+        if stream.peek() == c:
+            consume_in(stream, c)
+            return c
 
     # discard leading whitespace
     consume_whitespace(stream, peek_func=peek_strip)
 
-    # consume until whitespace (catching quoted text as well)
-    return consume_not_whitespace(stream, peek_func=peek_token)
+    # consume until whitespace or discrete token (catching quoted text as well)
+#    return consume_not_whitespace(stream, peek_func=peek_token)
+    return consume(stream, lambda c: not c.isspace() and c not in discrete_tokens, peek_func=peek_token)
 
-def tokens(stream):
+def tokens(stream, peek_func=peek, discrete_tokens=''):
     '''Yield tokens from a stream.'''
 
     class Reader:
@@ -124,26 +125,41 @@ def tokens(stream):
             return c
 
     stream = Reader(stream)
-    while token := consume_token(stream):
+    consume_whitespace(stream)
+    while token := consume_token(stream, peek_func, discrete_tokens):
         yield token
 
-def tokenize_string(string):
+def tokenize_string(string, peek_func=peek, discrete_tokens=''):
     '''Yield tokens from a string.'''
 
-    return tokens(io.StringIO(string))
+    return tokens(io.StringIO(string), peek_func, discrete_tokens)
 
 
 
 ### TABLE SCHEMAS ###
 
-# the spec string of tables themselves
-TABLE_SCHEMA = '"table name" version schema'
-
 class Schema:
     '''Represents a schema for a hierarchical table.'''
 
-    def __init__(self, spec: str):
+    @classmethod
+    def parse_tokens(cls, tokens: list):
+        '''Parse a schema from a token stream.'''
+
+        if tokens:
+            fields, rest = (tokens[:tokens.index(':')], tokens[tokens.index(':') + 1:]) if ':' in tokens else (tokens, [])
+            return cls(fields, cls.parse_tokens(rest))
+
+    @classmethod
+    def parse(cls, spec: str):
         '''Parse a schema from a spec string.'''
+
+        return cls.parse_tokens(list(tokenize_string(spec, discrete_tokens=':')))
+
+    def __init__(self, fields: list, child):
+        self.fields = fields
+        self.child = child
+
+table_schema = Schema.parse('"table name" version schema')
 
 
 
@@ -153,8 +169,45 @@ class Block:
     '''Represents a parsed block.'''
 
     @classmethod
-    def read(cls, stream: str, schema: Schema):
+    def read(cls, stream: str, schema: Schema = table_schema, level: int = 0):
         '''Parse a block from a stream according to a schema.'''
+
+        if schema:
+
+            # get the token stream
+            pos = stream.tell()
+            token_stream = tokens(stream)
+
+            # see what the first token is
+            try:
+                first_token = next(token_stream)
+            except StopIteration:
+                return
+
+            # if its a header level indicator
+            # then it indicates the level
+            # otherwise the level is simply one more than the last
+            if all(map(lambda c: c == '#', first_token)):
+                next_level = len(first_token)
+            else:
+                next_level = level + 1
+                stream.seek(pos)
+
+            # make sure we arent ascending (that would terminate the recursion)
+            if next_level > level:
+                header = dict(zip(schema.fields, token_stream))
+                child_schema = Schema.parse(header['schema']) if level == 0 else schema.child
+                children = list(cls.read_all(stream, child_schema, next_level))
+                return cls(header, children)
+            else:
+                stream.seek(pos)
+
+    @classmethod
+    def read_all(cls, stream: str, schema: Schema = table_schema, level: int = 0):
+        '''Read and yield all blocks from a stream.'''
+
+        while block := cls.read(stream, schema, level):
+            yield block
 
     @classmethod
     def parse(cls, string: str, schema: Schema):
@@ -166,6 +219,13 @@ class Block:
         self.header = header     # the header field values
         self.children = children # the sub-elements
 
+    @property
+    def dict(self):
+        return {**self.header, 'children': [child.dict for child in self.children]}
+
+    def __getitem__(self, item):
+        return self.dict[item]
+
 
 
 ### MAIN ###
@@ -175,5 +235,5 @@ if __name__ == '__main__':
         print('Please specify the path to the file you want to parse.')
     else:
         with open(sys.argv[1]) as f:
-            for token in tokens(f):
-                print(token)
+            import pprint
+            pprint.PrettyPrinter().pprint(list(block.dict for block in Block.read_all(f)))
